33974ae105c5938af65a06516c206b9b7e66a3e1
okadasorachi <Soraoka0418@outlook.jp>
2022-10-12 08:50:31 +0900

    modal実装

---
+++ components/TemplateModal.vue
@@ -0,0 +1,62 @@
+<template>
+  <transition class="modal">
+    <div class="modal__overlay" @click.self="$emit('close')">
+      <div class="modal__window">
+        <div class="modal__content">
+          <slot></slot>
+        </div>
+        <button class="modal-btn" @click="$emit('close')">閉じる</button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script></script>
+
+<style scoped>
+.modal__overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color:rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal__window {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  background-color: #fff;
+  border-radius: 20px;
+  overflow: hidden;
+}
+.modal__content {
+  padding: 20px;
+}
+
+.modal-btn {
+  width: 200px;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  border-radius: 20px;
+  padding: 10px 20px;
+  margin-top: auto;
+  margin-bottom: 20px;
+  background-color: rgb(209, 237, 237);
+}
+
+.modal-enter{
+  opacity: 0;
+}
+
+.modal-enter .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="modal">
+      <button class="button" @click="showModal">
+        モーダルの説明
+      </button>
+      <TemplateModal @close="closeModal" v-if="modal">
+        <template class="modal">
+          <p>slot: 要素の中に好きなものを入れられる</p>
+          <p>emit: 親要素にイベントを伝えられる</p>
+          <p>self: イベント要素が自分の時だけ呼び出される</p>
+        </template>
+      </TemplateModal>
+    </div>
+  </div>
+</template>
+
+<script>
+import TemplateModal from "../components/TemplateModal.vue";
+export default {
+  components: { TemplateModal },
+  data() {
+    return {
+      modal: false
+    };
+  },
+  methods: {
+    showModal() {
+      this.modal = true;
+    },
+    closeModal() {
+      this.modal = false;
+    }
+  },
+};
+</script>
+
+<style>
+.button {
+  font-size: 18px;
+  width: 200px;
+  height: 60px;
+  background-color: rgb(205, 241, 170);
+  outline: none;
+  border-radius: 15px;
+  border: none;
+}
+</style>
